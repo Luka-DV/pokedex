bbc57c4a187971eb9da67dcc49ad966f20e5ae90
Luka-DV <luka.guva@gmail.com>
2025-04-19 11:05:46 +0200

    add repl logic

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,37 @@
+import { rawListeners } from "node:process"
+
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+
 
 export function cleanInput(input: string): string[] {
-    return input.trim().split(" ");
+    return input
+        .trim()
+        .toLowerCase()
+        .split(" ")
+        
+}
+
+
+const rl = createInterface({
+  input: stdin,
+  output: stdout,
+  prompt: "Pokedex > "
+});
+
+
+
+export function startREPL() {
+
+    rl.prompt()
+
+    rl.on('line', (line) => {
+        const input = cleanInput(line);
+        if(input.length === 0) {
+            rl.prompt();
+            return
+        }
+        console.log(`Your command was: ${input[0]}`);
+        rl.prompt();
+});
 }
